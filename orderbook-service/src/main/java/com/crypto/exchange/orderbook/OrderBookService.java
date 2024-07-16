44ce56dcc1f0627836fcc8f8738be5e68a17d022
Anna Iakovleva <massadant@yandex.ru>
2024-07-17 00:10:44 +0200

    fix: split OrderBookService logic into two services to make @Transactional work

--- orderbook-service/src/main/java/com/crypto/exchange/orderbook/OrderBookService.java
+++ orderbook-service/src/main/java/com/crypto/exchange/orderbook/OrderBookService.java
@@ -1,18 +1,13 @@
 package com.crypto.exchange.orderbook;
 
 import com.crypto.exchange.models.http.OrderRequest;
-import com.crypto.exchange.models.OrderStatus;
 import com.crypto.exchange.models.Tuple;
 import com.crypto.exchange.models.events.OrdersMatched;
-import com.crypto.exchange.models.OrderType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.amqp.core.Queue;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Slf4j
 @Service
@@ -22,44 +17,30 @@ public class OrderBookService {
     private Queue orderQueue;
 
     @Autowired
-    private OrderRepository orderRepository;
+    private OrderTransactionalService orderTransactionalService;
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     public Order placeOrder(OrderRequest orderRequest) {
-        // Save to order book
-        Order order = new Order(
-                orderRequest.getUserId(),
-                orderRequest.getOrderType(),
-                orderRequest.getAmount(),
-                orderRequest.getPrice(),
-                orderRequest.getBaseCurrency(),
-                orderRequest.getQuoteCurrency(),
-                OrderStatus.PLACED
-        );
-        orderRepository.save(order);
-        log.info("Order with id '{}' was saved", order);
+        //Create and save new order
+        Order order = orderTransactionalService.createOrder(orderRequest);
 
         //Make a copy of newly created order for returning
-        Order savedOrder = new Order(order);
+        Order orderBeforeMatch = new Order(order);
 
-        // Try to match the order
+        //Try to match the order
         matchOrder(order);
 
-        return savedOrder;
+        //return the order with primary amount
+        return orderBeforeMatch;
     }
 
     private void matchOrder(Order order) {
         log.info("Matching order with id '{}' started", order.getId());
-        List<Order> matchingOrders = orderRepository.findMatchingOrders(
-                order.getOrderType() == OrderType.BUY ? OrderType.SELL : OrderType.BUY,
-                order.getBaseCurrency(),
-                order.getQuoteCurrency(),
-                order.getPrice()
-        );
 
-        matchingOrders
+        orderTransactionalService
+                .fetchMatchingOrders(order)
                 .stream()
                 .takeWhile(matchingOrder -> order.getAmount() > 0)
                 .forEachOrdered(matchingOrder -> {
@@ -69,7 +50,7 @@ public class OrderBookService {
                             order.getId(), matchingOrder.getId(), matchedAmount);
 
                     // Update orders
-                    updateOrders(order, matchingOrder, matchedAmount);
+                    orderTransactionalService.updateOrders(order, matchingOrder, matchedAmount);
 
                     log.info("Event about matching orders '{}' and '{}' is publishing",
                             order.getId(), matchingOrder.getId());
@@ -96,21 +77,5 @@ public class OrderBookService {
         );
         log.info("Sending OrdersMatched event: '{}'", ordersMatched);
         rabbitTemplate.convertAndSend(orderQueue.getName(), ordersMatched);
-    }
-
-    private void updateOrderStatusAndAmount(Order order, double matchedAmount) {
-        order.setAmount(order.getAmount() - matchedAmount);
-        if (order.getAmount() == 0) {
-            order.setStatus(OrderStatus.COMPLETED);
-        } else {
-            order.setStatus(OrderStatus.PARTIAL);
-        }
-        orderRepository.save(order);
-    }
-
-    @Transactional
-    private void updateOrders(Order currentOrder, Order matchingOrder, double matchedAmount) {
-        updateOrderStatusAndAmount(currentOrder, matchedAmount);
-        updateOrderStatusAndAmount(matchingOrder, matchedAmount);
     }
 }
---
+++ orderbook-service/src/main/java/com/crypto/exchange/orderbook/OrderTransactionalService.java
@@ -0,0 +1,62 @@
+package com.crypto.exchange.orderbook;
+
+import com.crypto.exchange.models.OrderStatus;
+import com.crypto.exchange.models.OrderType;
+import com.crypto.exchange.models.http.OrderRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class OrderTransactionalService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    Order createOrder(OrderRequest orderRequest) {
+        // Save to order book
+        Order order = new Order(
+                orderRequest.getUserId(),
+                orderRequest.getOrderType(),
+                orderRequest.getAmount(),
+                orderRequest.getPrice(),
+                orderRequest.getBaseCurrency(),
+                orderRequest.getQuoteCurrency(),
+                OrderStatus.PLACED
+        );
+        orderRepository.save(order);
+        log.info("Order with id '{}' was saved", order);
+
+        return order;
+    }
+
+    List<Order> fetchMatchingOrders(Order order) {
+        return orderRepository.findMatchingOrders(
+                order.getOrderType() == OrderType.BUY ? OrderType.SELL : OrderType.BUY,
+                order.getBaseCurrency(),
+                order.getQuoteCurrency(),
+                order.getPrice()
+        );
+    }
+
+    @Transactional
+    public void updateOrders(Order currentOrder, Order matchingOrder, double matchedAmount) {
+        updateOrderStatusAndAmount(currentOrder, matchedAmount);
+        updateOrderStatusAndAmount(matchingOrder, matchedAmount);
+    }
+
+    private void updateOrderStatusAndAmount(Order order, double matchedAmount) {
+        order.setAmount(order.getAmount() - matchedAmount);
+        if (order.getAmount() == 0) {
+            order.setStatus(OrderStatus.COMPLETED);
+        } else {
+            order.setStatus(OrderStatus.PARTIAL);
+        }
+        orderRepository.save(order);
+    }
+
+}
